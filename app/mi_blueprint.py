3962e5a4d20763f56c5e4c03f9902a841717b7c4
matt <danmsuarez111@soy.sena.edu.co>
2024-10-30 16:41:45 -0500

    Pdf se genera, falta redireccion o descarga

--- app/mi_blueprint.py
+++ app/mi_blueprint.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, send_file,redirect
+from .pdf_generator import generar_pdf
+
 
 #crear y configurar mmi blueprint 
 mi_blueprint = Blueprint('mi_blueprint',
@@ -10,5 +12,22 @@ mi_blueprint = Blueprint('mi_blueprint',
 
 def bienvinido():
     return 'Usa audifonos por favor'
+
+
+@mi_blueprint.route('/generar_pdf')
+def crear_pdf():
+    try:
+        contenido = "Este es un ejemplo de contenido para el PDF."
+        nombre_archivo = "ejemplo.pdf"  # Asegúrate de que este es solo el nombre del archivo
+        
+        # Generar el PDF
+        print("PDF generado")
+        ruta_pdf = generar_pdf(nombre_archivo, contenido)
+        
+        return send_file(ruta_pdf, as_attachment=True, mimetype='application/pdf')
+    except Exception as e:
+        print(f"Error al generar el PDF: {e}")  # Imprime el error para depuración
+        return "Error al generar el PDF"
+
 
 
---
+++ app/pdf_generator.py
@@ -0,0 +1,25 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import os
+
+def generar_pdf(nombre_archivo, contenido):        
+    # Establecer la ruta del archivo PDF
+    ruta_pdf = os.path.join(nombre_archivo)
+    
+    # Creamos un objeto Canvas para el PDF
+    c = canvas.Canvas(ruta_pdf, pagesize=letter)
+    width, height = letter
+    
+    #Contenido del pdf
+    c.drawString(100, height - 100, "Contenido del PDF:")
+    for i, linea in enumerate(contenido.splitlines(), start=1):
+        c.drawString(100, height - (100 + i * 20), linea)
+        
+    c.save()
+    return ruta_pdf
+    
+
+
+
+
+
